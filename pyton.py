30d6e2907caaabf1e06df860eb90fb51d1350135
equipogeorgos-hash <equipogeorgos@gmail.com>
2025-10-21 12:22:55 +0200

    Add files via upload
    suma

---
+++ pyton.py
@@ -0,0 +1,11 @@
+# Programa para sumar dos números
+
+# Pedir los números al usuario
+num1 = float(input("Introduce el primer número: "))
+num2 = float(input("Introduce el segundo número: "))
+
+# Calcular la suma
+suma = num1 + num2
+
+# Mostrar el resultado
+print("La suma es:", suma)
